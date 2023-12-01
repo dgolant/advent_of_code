69358464068e2e20a9d9d42f3a9a9d7cfc1ace79
Dan Golant <daniel.golant@datadoghq.com>
2023-12-01 14:49:23 -0500

    First

---
+++ 2023/d1/p1/target/debug/deps/p1-a2682823d19268b1.d
@@ -0,0 +1,5 @@
+/Users/daniel.golant/dev/advent_of_code/2023/d1/p1/target/debug/deps/p1-a2682823d19268b1: src/main.rs
+
+/Users/daniel.golant/dev/advent_of_code/2023/d1/p1/target/debug/deps/p1-a2682823d19268b1.d: src/main.rs
+
+src/main.rs:
---
+++ 2023/d1/p1/target/debug/p1.d
@@ -0,0 +1,1 @@
+/Users/daniel.golant/dev/advent_of_code/2023/d1/p1/target/debug/p1: /Users/daniel.golant/dev/advent_of_code/2023/d1/p1/src/main.rs
